a94a89f15b9cc0158a7c1fd28dc232fdeb1c53ce
Serg Up <ope04030@gmail.com>
2023-11-04 20:46:26 +0900

    upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
   css: ['~/assets/styles.scss'],
   modules: ['@pinia/nuxt'],
   devtools: { enabled: true },
-  generate: {
-    routes: ['/'],
-    async routes() {
-      const response = await fetch('https://devtwit8.ru/api/v1/page/?path=/');
-      const articles = await response.json();
-      return articles.map((article) => {
-        return {
-          route: `/${article.link}`,
-          payload: article,
-        };
-      });
-    },
-  },
 })
